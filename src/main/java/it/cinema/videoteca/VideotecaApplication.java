a9537c0012b32ecf41858c70baa32f4c0360a631
Edoardo Baral <edoardo@baral.it>
2021-01-02 22:41:08 +0100

    Formattazione classe VideotecaApplication

--- src/main/java/it/cinema/videoteca/VideotecaApplication.java
+++ src/main/java/it/cinema/videoteca/VideotecaApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideotecaApplication {
+public class VideotecaApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(VideotecaApplication.class, args);
 	}
-
 }
