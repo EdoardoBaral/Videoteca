bd17083a97138dd67dc87ba513378b9a136adf7e
Edoardo Baral <edoardo@baral.it>
2021-01-02 20:24:51 +0100

    Primo commit con la struttura di base dell'applicazione

---
+++ src/test/java/it/cinema/videoteca/VideotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cinema.videoteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
